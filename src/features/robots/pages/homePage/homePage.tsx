a6f3989228b4423e33a019fa9b85b806d3933574
Alina <alilaursdesign@gmail.com>
2022-11-20 18:12:02 +0100

    Adding HomePage

--- src/features/robots/pages/homePage/homePage.tsx
+++ src/features/robots/pages/homePage/homePage.tsx
@@ -1,9 +1,9 @@
 function HomePage() {
     return (
         <main>
-            <h2>Sales page</h2>
+            <h2>Robots</h2>
         </main>
     );
 }
 
-export default SalesPage;
+export default HomePage;
