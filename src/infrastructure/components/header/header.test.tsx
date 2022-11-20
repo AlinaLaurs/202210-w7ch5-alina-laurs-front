50d8f61c9ec549f9a1c4440bf09ddb4d1cafc0ef
Alina <alilaursdesign@gmail.com>
2022-11-20 18:29:07 +0100

    Adding header test

--- src/infrastructure/components/header/header.test.tsx
+++ src/infrastructure/components/header/header.test.tsx
@@ -1,17 +1,11 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter as Router } from 'react-router-dom';
 import { Header } from './header';
 
 describe('Given Header component', () => {
     describe('When we render the component', () => {
-        test('Then it should display "AllSaint"', () => {
-            render(
-                <Router>
-                    <Header />
-                </Router>
-            );
-            const element = screen.getByText(/AllSaint/i);
+        test('Then it should display "Robots"', () => {
+            render(<Header />);
+            const element = screen.getByText(/Robots/i);
             expect(element).toBeInTheDocument();
         });
     });
