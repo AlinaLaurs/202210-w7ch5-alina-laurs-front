91bfc92de8e55c40f73ebb15bda195b35448eddb
Alina <alilaursdesign@gmail.com>
2022-11-20 18:13:44 +0100

    Adding testing HomePage

--- src/features/robots/pages/homePage/homePage.test.tsx
+++ src/features/robots/pages/homePage/homePage.test.tsx
@@ -1,18 +1,13 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter as Router } from 'react-router-dom';
-import Sales from './homePage';
+import HomePage from './homePage';
 
 describe('Given SalesPage component', () => {
     describe('When we render the component', () => {
         beforeEach(() => {
-            render(
-                <Router>
-                    <Sales />
-                </Router>
-            );
+            render(<HomePage />);
         });
         test('Then it should display the title', () => {
-            const element = screen.getByText(/Sales/i);
+            const element = screen.getByText(/Robots/i);
             expect(element).toBeInTheDocument();
         });
     });
