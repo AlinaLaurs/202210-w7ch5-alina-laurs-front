63d9ea2f166b8936bf7e6b680fb58cf0c317f788
Alina <alilaursdesign@gmail.com>
2022-11-20 18:27:05 +0100

    Adding header

--- src/infrastructure/components/header/header.tsx
+++ src/infrastructure/components/header/header.tsx
@@ -1,12 +1,10 @@
-import { Menu } from '../menu/menu';
 import styles from './header.module.css';
 
 export function Header() {
-    const title = 'AllSaint Shop';
+    const title = 'Robots';
     return (
         <header className={styles.header}>
             <h1>{title}</h1>
-            <Menu></Menu>
         </header>
     );
 }
